d6fbc8180fe6ed88ad36c08e42c50d3d9051fcf3
syedaaleezaabbas <syedaaleezaabbas@gmail.com>
2024-10-17 11:56:00 -0700

    push my assignment2 to github

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -3,27 +3,27 @@ import Image from "next/image"
 export default function Footer() {
     return (
         <footer className="bg-blue-950 text-white py-8">
-            <div className="container mx-auto px-12 pb-6 flex">
+            <div className="container mx-auto md:px-12 px-6 pb-6 md:flex flex-row">
                 {/* left section */}
-                <div className="w-1/3 pr-4">
+                <div className="md:w-1/3 w-full pr-4">
                     <h3 className="text-2xl font-bold mt-3">Beauty Care</h3>
-                    <p className="mt-8">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eaque fugiat, velit unde, rerum tenetur molestias, quod error nesciunt ad autem minus.</p>
+                    <p className="mt-8">At Beauty Care, we believe in the power of simplicty and elegance. Our curated colection of furniture is crafted to elevate your home, offering timeless designs that blend seamlessly with modern aesthetics. Explore our selection and experience the beauty and comfort we bring to every space.</p>
                     <h3 className="text-2xl font-bold mt-4">Follow Us</h3>
                 </div>
                 {/* right section */}
-                <div className="flex-1 w-1/3">
-                    <h3 className="text-2xl font-bold mt-6">Instagram Shop</h3>
+                <div className="flex-1 md:w-1/3 w-full">
+                    <h3 className="text-2xl font-bold md:mt-6 mt-10">Instagram Shop</h3>
                     <div className="flex justify-center items-center space-x-4 mt-2">
-                        <div className="space-x-2 mt-2">
+                        <div className="space-x-2 mt-2 transition-transform duration-300 hover:scale-105 hover:shadow-lg">
                             <Image src="/footerpic1.jpg" alt="instagram1" width={550} height={550} />
                         </div>
-                        <div className="space-x-2 mt-2">
+                        <div className="space-x-2 mt-2 transition-transform duration-300 hover:scale-105 hover:shadow-lg">
                             <Image src="/footerpic2.jpg" alt="instagram2" width={550} height={550} />
                         </div>
-                        <div className="space-x-2 mt-2">
+                        <div className="space-x-2 mt-2 transition-transform duration-300 hover:scale-105 hover:shadow-lg">
                             <Image src="/footerpic3.jpg" alt="instagram3" width={550} height={150} />
                         </div>
-                        <div className="space-x-2 mt-2">
+                        <div className="space-x-2 mt-2 transition-transform duration-300 hover:scale-105 hover:shadow-lg">
                             <Image src="/footerpic4.jpg" alt="instagram4" width={550} height={550} />
                         </div>
                     </div>
--- src/app/header.tsx
+++ src/app/header.tsx
@@ -3,7 +3,7 @@ import { RxHamburgerMenu } from "react-icons/rx";
 
 export default function Header() {
     return (
-        <header className="flex justify-between items-center bg-white py-4 px-5 md:px-20 shadow-md">
+        <header className="flex justify-between items-center bg-white py-4 px-5 md:px-20 md:pl-32 shadow-md">
             <div className="flex justify-between items-center w-full">
                 <h1 className="text-2xl md:text-3xl font-bold text-blue-950">Furniture</h1>
             </div>
@@ -14,16 +14,15 @@ export default function Header() {
 
             <div>
                 <nav className="hidden md:flex space-x-12">
-                    <ul className="flex gap-x-12">
-                        <li><Link href="/" className="text-teal-600 hover:text-black hover:underline">Home</Link></li>
-                        <li><Link href="/services" className="text-teal-600 hover:text-black hover:underline">Services</Link></li>
-                        <li><Link href="/products" className="text-teal-600 hover:text-black hover:underline">Products</Link></li>
-                        <li><Link href="/gallery" className="text-teal-600 hover:text-black hover:underline">Gallery</Link></li>
-                        <li><Link href="/contact" className="text-teal-600 hover:text-black hover:underline">Contact</Link></li>
+                    <ul className="flex gap-x-16 pr-14">
+                        <li><Link href="/" className="text-gray-600 hover:text-teal-600 hover:underline">Home</Link></li>
+                        <li><Link href="/services" className="text-gray-600 hover:text-teal-600 hover:underline">Services</Link></li>
+                        <li><Link href="/products" className="text-gray-600 hover:text-teal-600 hover:underline">Products</Link></li>
+                        <li><Link href="/gallery" className="text-gray-600 hover:text-teal-600 hover:underline">Gallery</Link></li>
+                        <li><Link href="/contact" className="text-gray-600 hover:text-teal-600 hover:underline">Contact</Link></li>
                     </ul>
                 </nav>
             </div>
-
         </header>
     )
 }
